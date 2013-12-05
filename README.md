1f80ab015f1f95ce5907f7e11157462669189ebe
yoann <yoann.prot@afp.com>
2013-12-05 11:03:32 +0100

    First commit - test and docs on going

--- README.md
+++ README.md
@@ -1,11 +1,21 @@
-ui-select2   [![Build Status](https://travis-ci.org/angular-ui/ui-select2.png)](https://travis-ci.org/angular-ui/ui-select2)
+ui-select2-sortable   [![Build Status](https://travis-ci.org/Taranys/ui-select2-sortable.png?branch=master)](https://travis-ci.org/Taranys/ui-select2-sortable)
 ==========
 This directive allows you to enhance your select elements with behaviour from the [select2](http://ivaynberg.github.io/select2/) library.
+
+It is a fork from [ui-select2](https://github.com/angular-ui/ui-select2).
+
+I change it to meet my needs :
+* Multiple elements
+* Sortable with drag&grop
+* Use any JS objects as data without any conversion between select2 model and your model
+
+I removed support of <select> to keep only <input type="hidden"> to simplify the directive.
 
 # Requirements
 
 - [AngularJS](http://angularjs.org/)
 - [JQuery](http://jquery.com/)
+- [JQueryUi](http://jqueryui.com/)
 - [Select2](http://ivaynberg.github.io/select2/)
 
 ## Setup
@@ -16,12 +26,10 @@ This directive allows you to enhance your select elements with behaviour from th
   `$ npm install`
 3. Install components
   `$ bower install`
-4. ???
-5. Profit!
 
 ## Testing
 
-We use [Grunt](http://gruntjs.com/) to check for JavaScript syntax errors and execute all unit tests. To run Grunt, simply execute:
+I use [Grunt](http://gruntjs.com/) to check for JavaScript syntax errors and execute all unit tests. To run Grunt, simply execute:
 
 `$ grunt`
 
@@ -33,19 +41,20 @@ This will start a Karma server in the background and run unit tests in Firefox a
 
 # Usage
 
-We use [bower](https://github.com/bower/bower) for dependency management. Install AngularUI Select2 into your project by running the command
+I use [bower](https://github.com/bower/bower) for dependency management. Install AngularUI Select2 Sortable into your project by running the command
 
-`$ bower install angular-ui-select2`
+`$ bower install angular-ui-select2-sortable`
 
-If you use a `bower.json` file in your project, you can have Bower save ui-select2 as a dependency by passing the `--save` or `--save-dev` flag with the above command.
+If you use a `bower.json` file in your project, you can have Bower save ui-select2-sortable as a dependency by passing the `--save` or `--save-dev` flag with the above command.
 
-This will copy the ui-select2 files into your `bower_components` folder, along with its dependencies. Load the script files in your application:
+This will copy the ui-select2-sortable files into your `bower_components` folder, along with its dependencies. Load the script files in your application:
 ```html
 <link rel="stylesheet" href="bower_components/select2/select2.css">
 <script type="text/javascript" src="bower_components/jquery/jquery.js"></script>
+<script type="text/javascript" src="bower_components/jquery/jquery-ui.js"></script>
 <script type="text/javascript" src="bower_components/select2/select2.js"></script>
 <script type="text/javascript" src="bower_components/angular/angular.js"></script>
-<script type="text/javascript" src="bower_components/angular-ui-select2/src/select2.js"></script>
+<script type="text/javascript" src="bower_components/angular-ui-select2/src/select2sortable.js"></script>
 ```
 
 (Note that `jquery` must be loaded before `angular` so that it doesn't use `jqLite` internally)
@@ -54,44 +63,15 @@ This will copy the ui-select2 files into your `bower_components` folder, along w
 Add the select2 module as a dependency to your application module:
 
 ```javascript
-var myAppModule = angular.module('MyApp', ['ui.select2']);
+var myAppModule = angular.module('MyApp', ['ui.select2.sortable']);
 ```
 
 Apply the directive to your form elements:
 
 ```html
+TODO
 <select ui-select2 ng-model="select2" data-placeholder="Pick a number">
     <option value=""></option>
-    <option value="one">First</option>
-    <option value="two">Second</option>
-    <option value="three">Third</option>
-</select>
-```
-
-## Options
-
-All the select2 options can be passed through the directive. You can read more about the supported list of options and what they do on the [Select2 Documentation Page](http://ivaynberg.github.com/select2/)
-
-```javascript
-myAppModule.controller('MyController', function($scope) {
-    $scope.select2Options = {
-        allowClear:true
-    };
-});
-```
-
-```html
-<select ui-select2="select2Options" ng-model="select2">
-    <option value="one">First</option>
-    <option value="two">Second</option>
-    <option value="three">Third</option>
-</select>
-```
-
-Some time it may make sense to specify the options in the template file.
-
-```html
-<select ui-select2="{ allowClear: true}" ng-model="select2">
     <option value="one">First</option>
     <option value="two">Second</option>
     <option value="three">Third</option>
@@ -107,43 +87,18 @@ If you add the ng-model directive to same the element as ui-select2 then the pic
 ## Working with dynamic options
 `ui-select2` is incompatible with `<select ng-options>`. For the best results use `<option ng-repeat>` instead.
 ```html
+TODO
 <select ui-select2 ng-model="select2" data-placeholder="Pick a number">
     <option value=""></option>
     <option ng-repeat="number in range" value="{{number.value}}">{{number.text}}</option>
 </select>
 ```
 
-## Working with placeholder text
-In order to properly support the Select2 placeholder, create an empty `<option>` tag at the top of the `<select>` and either set a `data-placeholder` on the select element or pass a `placeholder` option to Select2.
-```html
-<select ui-select2 ng-model="number" data-placeholder="Pick a number">
-    <option value=""></option>
-    <option value="one">First</option>
-    <option value="two">Second</option>
-    <option value="three">Third</option>
-</select>
-```
+## Using simple query mode
 
-## ng-required directive
-
-If you apply the required directive to element then the form element is invalid until an option is selected.
-
-Note: Remember that the ng-required directive must be explicitly set, i.e. to "true".  This is especially true on divs:
-
-```html
-<select ui-select2 ng-model="number" data-placeholder="Pick a number" ng-required="true">
-    <option value=""></option>
-    <option value="one">First</option>
-    <option value="two">Second</option>
-    <option value="three">Third</option>
-</select>
-```
-
-## Using simple tagging mode
-
-When AngularJS View-Model tags are stored as a list of strings, setting
-the ui-select2 specific option `simple_tags` will allow to keep the model
-as a list of strings, and not convert it into a list of Select2 tag objects.
+When AngularJS View-Model tags are stored as a list of strings,
+setting the ui-select2 specific option `simple_tags` will allow to keep the model as a list of strings,
+and not convert it into a list of Select2 tag objects.
 
 ```html
 <input
---
+++ src/select2sortable.js
@@ -0,0 +1,182 @@
+/**
+ * Enhanced Select2 Dropmenus
+ *
+ * @AJAX Mode - When in this mode, your value will be an object (or array of objects) of the data used by Select2
+ *     This change is so that you do not have to do an additional query yourself on top of Select2's own query
+ * @params [options] {object} The configuration options passed to $.fn.select2(). Refer to the documentation
+ */
+angular.module('ui.select2.sortable', []).directive('uiSelect2Sortable', ['$timeout', function ($timeout) {
+    return {
+        require: 'ngModel',
+        restrict: 'A',
+        transclude: true,
+        scope: {
+            ngModel: '=',
+            allowClear: '=?',
+            simpleQuery: '=?',
+            query: '=?',
+            toId: '=?',
+            toText: '=?',
+            minimumInputLength: '=?',
+            onSelect: '=?'
+        },
+        link: function (scope, element, attrs, ngModel) {
+            //create a function to find an id into object
+            if (!scope.toId) {
+                scope.toId = function (item) {
+                    if (item._id) return item._id;
+                    if (item.id) return item.id;
+                    if (item.uri) return item.uri;
+                    if (item.href) return item.href;
+                    if (item.href) return item.resource;
+                    return item;
+                };
+            }
+
+            //create a function to find display value into object
+            if (!scope.toText) {
+                scope.toText = function (item) {
+                    if (item.text) return item.text;
+                    if (item.name) return item.name;
+                    if (item.label) return item.label;
+                    return item;
+                };
+            }
+
+            //prepare options for the select2 element
+            scope.opts = {
+                multiple: attrs.multiple || false,
+                sortable: attrs.multiple || false,
+                minimumInputLength: scope.minimumInputLength || 0,
+                query: scope.query,
+                allowClear: scope.allowClear || false
+            };
+
+            // Convert from Select2 view-model to Angular view-model.
+            scope.convertToAngularModel = function (select2_data) {
+                var model;
+                if (angular.isArray(select2_data)) {
+                    model = [];
+                    angular.forEach(select2_data, function (value) {
+                        model.push(value._data);
+                    });
+                } else {
+                    if (select2_data && select2_data._data) {
+                        model = select2_data._data;
+                    } else {
+                        model = select2_data;
+                    }
+                }
+                return model;
+            };
+
+            // Convert from Angular view-model to Select2 view-model.
+            scope.convertToSelect2Model = function (angular_data) {
+                if (angular.isArray(angular_data)) {
+                    var model = [];
+                    angular.forEach(
+                        angular_data,
+                        function (value) {
+                            model.push({
+                                id: scope.toId(value),
+                                text: scope.toText(value),
+                                _data: value
+                            });
+                        });
+                    return model;
+                } else if (angular.isObject(angular_data)) {
+                    return {
+                        id: scope.toId(angular_data),
+                        text: scope.toText(angular_data),
+                        _data: angular_data
+                    };
+                }
+
+                return angular_data;
+            };
+
+            //allow user to create a simple query
+            //just use simpleQuery : function(term, callback) { callback(array_of_dummy_objects; }
+            if (scope.simpleQuery) {
+                scope.opts.query = function (query) {
+                    scope.simpleQuery(query.term, function (values) {
+                        query.callback({ results: scope.convertToSelect2Model(values) });
+                    });
+                };
+            }
+
+            // call select2 function to set data and all properties
+            scope.render = function () {
+                if (scope.opts.multiple) {
+                    element.select2('data', scope.convertToSelect2Model(ngModel.$viewValue));
+                } else {
+                    if (angular.isObject(ngModel.$viewValue)) {
+                        element.select2('data', scope.convertToSelect2Model(ngModel.$viewValue));
+                    } else if (!ngModel.$viewValue) {
+                        element.select2('data', null);
+                    } else {
+                        element.select2('val', scope.convertToSelect2Model(ngModel.$viewValue));
+                    }
+                }
+                if (scope.opts.sortable) {
+                    element.select2("container").find("ul.select2-choices").sortable({
+                        containment: 'parent',
+                        start: function () {
+                            element.select2("onSortStart");
+                        },
+                        update: function () {
+                            element.select2("onSortEnd");
+                            element.trigger('change');
+                        }
+                    });
+                }
+            };
+
+            // Set the view and model value and update the angular template manually for the ajax/multiple select2.
+            element.bind("change", function () {
+                if (scope.$$phase) {
+                    return;
+                }
+                scope.$apply(function () {
+                    var values = element.select2('data');
+                    if (values && (angular.isArray(values) || values._data)) {
+                        values = scope.convertToAngularModel(values);
+                    }
+                    ngModel.$setViewValue(values);
+                });
+            });
+
+            // Watch the model for programmatic changes
+            scope.$watch(function () {
+                return ngModel.$viewValue;
+            }, function (current, old) {
+                if (current === old) return;
+                scope.render();
+            }, true);
+
+            element.bind("$destroy", function () {
+                element.select2("destroy");
+            });
+
+            //watch disabled attrs to send event to select2
+            attrs.$observe('disabled', function (value) {
+                element.select2('enable', !value);
+            });
+
+            // add an onSelect element which retreive model object into e.added or e.removed
+            if (scope.onSelect) {
+                element.on("change", function (e) {
+                    if (e.added) e.added = scope.convertToAngularModel(e.added);
+                    if (e.removed) e.removed = scope.convertToAngularModel(e.removed);
+                    scope.onSelect(e);
+                });
+            }
+
+            // Initialize the plugin late so that the injected DOM does not disrupt the template compiler
+            $timeout(function () {
+                element.select2(scope.opts);
+                scope.render();
+            });
+        }
+    };
+}]);
